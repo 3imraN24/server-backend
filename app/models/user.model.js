e87a289a2dfbdc577365cd77145fa4cc59627d89
3imraN24 <166580511+3imraN24@users.noreply.github.com>
2025-05-02 04:19:20 +0100

    Update user.model.js

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,14 +1,66 @@
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("users", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
     username: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
     },
     email: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    phone: {
+      type: Sequelize.STRING,
+      allowNull: true
     },
     password: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    birth_date: {
+      type: Sequelize.DATEONLY,
+      allowNull: true
+    },
+    country: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    city: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    school: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    university: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    class: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    time_zone: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    device_info: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
     }
+  }, {
+    timestamps: false // لأننا نستخدم created_at يدويًا
   });
 
   return User;
